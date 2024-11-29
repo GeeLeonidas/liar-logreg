5ee44f991e865588add6daeae493d6ace9a7de86
Guilherme Leoi <geeleonidas@pm.me>
2024-11-29 15:20:48 -0300

    Extract and load the downloaded dataset

--- liar_logreg/__main__.py
+++ liar_logreg/__main__.py
@@ -4,6 +4,12 @@
 
 import traceback
 
+import os
+import pandas as pd
+from zipfile import ZipFile
+    
+from . import download_liar_dataset
+
 
 CODE_OK = 0
 CODE_ERROR = 1
@@ -11,6 +17,15 @@ CODE_ERROR = 1
 
 def main() -> int:
     try:
+        if not os.path.isfile("./liar_dataset.zip"):
+            download_liar_dataset()
+        zipped_dataset = ZipFile("./liar_dataset.zip")
+        dataset = {
+            zipped_file.filename:
+                pd.read_csv(zipped_dataset.open(zipped_file.filename), sep='\t')
+            for zipped_file in zipped_dataset.infolist()
+            if zipped_file.filename.endswith(".tsv")
+        }
         #...
         
         return CODE_OK
