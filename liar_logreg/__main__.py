b5d1c3a47e792c620f7f07d1aaa5b7795346e55f
Guilherme Leoi <geeleonidas@pm.me>
2024-11-29 18:57:24 -0300

    Prepare the data

--- liar_logreg/__main__.py
+++ liar_logreg/__main__.py
@@ -8,6 +8,8 @@ from enum import IntEnum
 import os
 import pandas as pd
 from zipfile import ZipFile
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
     
 from . import download_liar_dataset
 
@@ -44,6 +46,27 @@ def main() -> Status:
             for zipped_file in zipped_dataset.infolist()
             if zipped_file.filename.endswith(".tsv")
         }
+        
+        train_df = dataset["train.tsv"]
+        test_df = dataset["test.tsv"]
+        valid_df = dataset["valid.tsv"]
+                
+        tf_idf = TfidfVectorizer() \
+            .fit(train_df["statement"]) \
+            .fit(test_df["statement"]) \
+            .fit(valid_df["statement"])
+        le = LabelEncoder() \
+            .fit(train_df["label"]) \
+            .fit(test_df["label"]) \
+            .fit(valid_df["label"])
+        
+        X_train = tf_idf.transform(train_df["statement"])
+        y_train = le.transform(train_df["label"])
+        X_test = tf_idf.transform(test_df["statement"])
+        y_test = le.transform(test_df["label"])
+        X_valid = tf_idf.transform(valid_df["statement"])
+        y_valid = le.transform(valid_df["label"])
+        
         #...
         
         return Status.OK
